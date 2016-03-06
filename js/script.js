9f7124f3302f775dd90510b8830d40b1bb551aba
jrs32 <jrs32@iastate.edu>
2016-03-05 16:00:53 -0800

    Front-End work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
+/* Executes when document is loaded */
+$(document).ready(function(){
+	// Load Tooptips
+    $('[data-toggle="tooltip"]').tooltip();   
+	setSidePanel(document.getElementById("welcome-block"));
+});
+
 /* Displays/Hides Grid Navigation Window */
 function showGridPopup(){
 	if(document.getElementById("grid-menu-popup").style.display == "none"){
 		document.getElementById("grid-menu-popup").style.display= "block";
-		document.getElementById("navbar-button").className = "grid-menu-icon grid-button-active";
+		document.getElementById("grid-navbar-button").className = "top-nav-button grid-button-active";
 	}
 	else {
 		document.getElementById("grid-menu-popup").style.display = "none";
-		document.getElementById("navbar-button").className = "grid-menu-icon grid-button-inactive";
+		document.getElementById("grid-navbar-button").className = "top-nav-button grid-button-inactive";
 	}
 }
 
@@ -19,4 +26,22 @@ function setSidePanel(element){
 	}
 	// Show selected panel
 	element.style.display = "block";
+}
+
+function setSidePanelDisplay(){
+	if(document.getElementById("overlay-container").style.display == "none"){
+		document.getElementById("overlay-container").style.display = "block";
+		document.getElementById("collapse-panel-button").style.background = "url(images/arrow-right.png)";
+		document.getElementById("collapse-panel-button").style.right = "31%";
+		$(document.getElementById("collapse-panel-button")).tooltip('hide').attr('data-original-title', 'Collapse Side Panel').tooltip('show');
+	}
+	else {
+		document.getElementById("overlay-container").style.display = "none";
+		document.getElementById("collapse-panel-button").style.background = "url(images/arrow-left.png)";
+		document.getElementById("collapse-panel-button").style.right = "0%";
+		$(document.getElementById("collapse-panel-button")).tooltip('hide').attr('data-original-title', 'Show Side Panel').tooltip('show');
+		  
+		  //          .tooltip('fixTitle')
+		//document.getElementById("collapse-panel-button").data-original-title = "Show Side Panel";
+	}
 }
